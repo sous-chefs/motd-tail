1494b628356049645e0e80faa7fcb8ddb283c3d4
Sam Kottler <shk@redhat.com>
2014-03-27 23:11:29 +0600

    libraries/provider_motd_tail.rb quietly requires motd, so add it to metadata.rb

--- metadata.rb
+++ metadata.rb
@@ -4,9 +4,11 @@ maintainer_email 'cookbooks@opscode.com'
 license          'Apache 2.0'
 description      'Updates motd.tail with Chef Roles'
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
-version          '2.0.1'
+version          '2.0.2'
 
 recipe           'motd-tail', 'Updates motd.tail with useful node data'
+
+depends          'motd'
 
 %w(debian ubuntu).each do |os|
   supports os
