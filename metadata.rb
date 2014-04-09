e7b77c3b13324a6d6af7f682b61250d7de2c42de
Sean OMeara <someara@opscode.com>
2014-04-09 11:43:43 -0400

    Version bump to 2.0.3 (for development)

--- metadata.rb
+++ metadata.rb
@@ -4,7 +4,7 @@ maintainer_email 'cookbooks@opscode.com'
 license          'Apache 2.0'
 description      'Updates motd.tail with Chef Roles'
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
-version          '2.0.2'
+version          '2.0.3'
 
 recipe           'motd-tail', 'Updates motd.tail with useful node data'
 
