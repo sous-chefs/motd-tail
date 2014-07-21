dd079e10c8520d3e4d0af49ca7a7bd07a7470390
Joshua Miller <joshua.miller@ticketfly.com>
2014-07-21 16:27:20 -0700

    useless change to trigger webhook

--- metadata.rb
+++ metadata.rb
@@ -2,7 +2,7 @@ name             'motd-tail'
 maintainer       'Opscode, Inc.'
 maintainer_email 'cookbooks@opscode.com'
 license          'Apache 2.0'
-description      'Updates motd.tail with Chef Roles'
+description      'Updates motd.tail with Chef Roles.'
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
 version          '2.0.3'
 
