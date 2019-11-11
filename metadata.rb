eb1e8fac90bcccf3f2efd775341d0518708f9dd8
Tim Smith <tsmith84@gmail.com>
2019-11-10 17:31:23 -0800

    Remove long_description and recipe metadata
    Neither of these do anything
    
    Signed-off-by: Tim Smith <tsmith@chef.io>

--- metadata.rb
+++ metadata.rb
@@ -3,10 +3,7 @@ maintainer 'Chef Software, Inc.'
 maintainer_email 'cookbooks@chef.io'
 license 'Apache-2.0'
 description 'Updates motd.tail with Chef Roles'
-long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
 version '5.1.0'
-
-recipe 'motd-tail', 'Updates motd.tail with useful node data'
 
 %w(debian ubuntu).each do |os|
   supports os
